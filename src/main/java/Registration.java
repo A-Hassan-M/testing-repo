4a84edac1d2f28db8dfdec6ee1005d74d20a2295
A-Hassan-M <a.hassanmahmoud0@gmail.com>
2023-04-27 21:33:22 +0200

    deleted files

--- src/main/java/Registration.java
+++
@@ -1,9 +0,0 @@
-public class Registration {
-    public void register(String roomId) {
-        System.out.println("registering room with id: " + roomId);
-    }
-
-    public void unregister(String guestName){
-        System.out.println("Unregistering guest with name: " + guestName);
-    }
-}
